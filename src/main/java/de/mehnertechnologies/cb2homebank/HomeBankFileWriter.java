d9ea3017bd1ad7cd61a17f0474e52b7bacfbceda
Björn Mehner <code@mehner-technologies.de>
2016-08-13 13:25:02 +0200

    Restructured command line parsing

--- src/main/java/de/mehnertechnologies/cb2homebank/HomeBankFileWriter.java
+++ src/main/java/de/mehnertechnologies/cb2homebank/HomeBankFileWriter.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.mehnertechnologies.cb2homebank;
 
 import java.util.List;
 
 /**
- *
- * @author bodo
+ * Writes a csv file in HomeBank format.
+ * @author bjoern.mehner
  */
 public class HomeBankFileWriter {
 
--- src/main/java/de/mehnertechnologies/cb2homebank/Main.java
+++ src/main/java/de/mehnertechnologies/cb2homebank/Main.java
@@ -1,7 +1,6 @@
 package de.mehnertechnologies.cb2homebank;
 
 import java.util.List;
-import org.apache.commons.cli.*;
 
 /**
  * Main class to start the converter.
@@ -11,7 +10,8 @@ public class Main {
     public static void main(String[] args) {
         
         try {
-            Cb2HbConfiguration configuration = parseCommandlineArgs(args);
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            Cb2HbConfiguration configuration = configurationBuilder.buildFromCliArguments(args);
      
             CbCsvFileReader cbCsvFileReader = new CbCsvFileReader(configuration);
             HomeBankFileWriter homeBankFileWriter = new HomeBankFileWriter(configuration);
@@ -24,43 +24,5 @@ public class Main {
             e.printStackTrace();
             System.exit(1);
         }
-    }
-
-    private static Cb2HbConfiguration parseCommandlineArgs(String[] args) {
-        Options options = createOptions();
-        
-        CommandLineParser parser = new DefaultParser();
-        CommandLine line = null;
-        
-        try {
-            line = parser.parse(options, args);
-        } catch (ParseException ex) {
-            help(options);
-            System.exit(1);
-        }
-        
-        String inputFile = line.getOptionValue("if");
-        String outputDirectory = line.getOptionValue("d");
-        
-        return new Cb2HbConfiguration(inputFile, outputDirectory);
-    }
-
-    private static void help(Options options) {
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("java -jar cb2homebank.jar", options);
-    }
-
-    private static Options createOptions() {
-        Options options = new Options();
-        
-        options.addOption("help", "Prints this help");
-        Option option1 = new Option("if", true, "Path to Commerzbank CSV input file (required");
-        option1.setRequired(true);
-        options.addOption(option1);
-        Option option2 = new Option("d", true, "Path to output directory (required)");
-        option2.setRequired(true);
-        options.addOption(option2);
-        
-        return options;
     }
 }
